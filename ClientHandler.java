025f51998bd571e880431eea77f2b3f7dcc8dbaf
chendyreva <nat-217@mail.ru>
2018-07-26 13:48:37 +0500

    обработчик клиента

---
+++ ClientHandler.java
@@ -0,0 +1,87 @@
+package ru.geekbrains.homework7;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    private MyServer myServer;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String name;
+    private String nickMsg;
+
+
+
+    public String getName() {
+        return name;
+
+    }
+
+    public ClientHandler(MyServer myServer, Socket socket) {
+        try {
+            this.myServer = myServer;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            this.nickMsg = "/nick2";
+            this.name = "";
+            new Thread(() -> {
+                try {
+                    while (true) { // цикл авторизации
+                        String str = in.readUTF();
+                        if (str.startsWith("/auth")) {
+                            String[] parts = str.split("\\s");
+                            String nick =
+                                    myServer.getAuthService().getNickByLoginPass(parts[1], parts[2]);
+                            if (nick != null) {
+                                if (!myServer.isNickBusy(nick)) {
+                                    sendMsg("/authok " + nick);
+                                    name = name;
+                                    myServer.broadcastMsg(name + " зашел в чат");
+                                    myServer.subscribe(this);
+                                    break;
+                                } else sendMsg("Учетная запись уже используется");
+                            } else {
+                                sendMsg("Неверные логин/пароль");
+                            }
+                        }
+                    }
+                    while (true) { // цикл получения сообщений
+                        String str = in.readUTF();
+                        System.out.println("от " + name + ": " + str);
+                        if (str.equals("/end")) break;
+                        myServer.broadcastMsg(name + ": " + str);
+                        myServer.privateMsg(nickMsg + ": " + str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    myServer.unsubscribe(this);
+                    myServer.broadcastMsg(name + " вышел из чата");
+                    myServer.privateMsg(name + " вышел из чата");
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (IOException e) {
+            throw new RuntimeException("Проблемы при создании обработчика клиента");
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
